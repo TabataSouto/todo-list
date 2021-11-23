55ace5367fed1ebc7451ec83030d659de0e2208c
Tabata <tabatac.souto@gmail.com>
2021-11-23 18:30:36 -0300

    Requisito 07 + css: finalizado

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 // CRIAR NOVO ITEM DE TAREGA AO CLICAR NO BOTÃO
 const button = document.querySelector('#criar-tarefa');
+const inputTextoTarefa = document.querySelector('#texto-tarefa');
+const listaTarefas = document.querySelector('#lista-tarefas');
 
 // 1) capturar informação input e adi
 function addTarefa() {
   // pegou o resultado do input
-  const inputTextoTarefa = document.querySelector('#texto-tarefa');
   const inputValue = inputTextoTarefa.value;
+
   // criou a li pra cada tarefa adiciona no ol que é o pai da li
-  const listaTarefas = document.querySelector('#lista-tarefas');
   const liList = document.createElement('li');
+
+  // mudar a cor de fundo de cada li selecionado
+  liList.addEventListener('click', function() {
+    liList.style.backgroundColor = 'rgb(128, 128, 128)';
+  });
+
+  // adiciona de fato o elemento com o valor de input do usuário na página
   liList.innerText = inputValue;
+  liList.classList = 'item';
   listaTarefas.appendChild(liList);
-  // 3) ao clicar no botão "adicionar" a tarefa vai pra lista e some da caixa de input;
+
+  // ao clicar no botão "adicionar" a tarefa vai pra lista e some da caixa de input;
   inputTextoTarefa.value = '';
 }
 button.addEventListener('click', addTarefa);
