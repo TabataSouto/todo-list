2a9536386da3549e6bd3e760d5ce2d3bb0596b9f
Tabata <tabatac.souto@gmail.com>
2021-11-23 21:44:09 -0300

    Requisito 07 e 08: finalizado

--- script.js
+++ script.js
@@ -11,18 +11,24 @@ function addTarefa() {
   // c) criar a li para adicionar cada tarefa como li (filho do ol)
   const liList = document.createElement('li');
 
-  // d) mudar a cor de fundo de cada li quando selecionado
-  liList.addEventListener('click', function() {
-    liList.style.backgroundColor = 'rgb(128, 128, 128)';
-  });
-
   // e) adicionar de fato o elemento com o valor de input do usuário na página com a informação inputada do usuário
   liList.innerText = inputValue;
+
+  // função para mudar a cor do bg da tarefa selecionada
+  liList.addEventListener('click', addRemove);
   listaTarefas.appendChild(liList);
 
-// f) valor preenchido é apagado da caixa de texto ao clicar no botão de adicionar;
+  // f) valor preenchido é apagado da caixa de texto ao clicar no botão de adicionar;
   inputTextoTarefa.value = '';
 }
 button.addEventListener('click', addTarefa);
 
 // PINTAR UMA TAREFA POR VEZ
+function addRemove(event) {
+  const taskName = event.target;
+  const classSelected = document.querySelector('.selected');
+  if (classSelected) {
+    classSelected.classList.remove('selected');
+  }
+  taskName.classList.add('selected');
+}
