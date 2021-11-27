e42045c913c06475c4cdd48b03911ccf81462f9d
Tabata <tabatac.souto@gmail.com>
2021-11-27 17:48:19 -0300

    Requisito 12 e 13: quase finalizados

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const listaTarefas = document.querySelector('#lista-tarefas');
 const buttonDelete = document.querySelector('#apaga-tudo');
 const buttonRemFinal = document.querySelector('#remover-finalizados');
 const buttonRenSel = document.querySelector('#remover-selecionado');
+// const buttonSalTar = document.querySelector('#salvar-tarefas');
+const buttonMoverCima = document.querySelector('#mover-cima');
+const buttonMoverBaixo = document.querySelector('#mover-baixo');
 
 // a) capturar informação input
 // b) pegar o resultado do input
@@ -12,7 +15,6 @@ const buttonRenSel = document.querySelector('#remover-selecionado');
 // e) adicionar de fato o elemento com o valor de input do usuário na página com a informação inputada do usuário
 // f) função para mudar a cor do bg da tarefa selecionada
 // g) valor preenchido é apagado da caixa de texto ao clicar no botão de adicionar;
-
 function addTarefa() {
   const inputValue = inputTextoTarefa.value;
   const liList = document.createElement('li');
@@ -20,10 +22,12 @@ function addTarefa() {
 
   liList.addEventListener('click', addRemove);
   liList.addEventListener('dblclick', riscarTarefa);
+
   listaTarefas.appendChild(liList);
   inputTextoTarefa.focus();
   inputTextoTarefa.value = '';
 }
+
 button.addEventListener('click', addTarefa);
 
 // PINTAR UMA TAREFA POR VEZ
@@ -37,7 +41,7 @@ function addRemove(event) {
 }
 
 // RISCAR A TAREFA CLICADA DUAS VEZES
-// referencia: @SrTonn para entendimento e realização do requisito
+// referencia: @SrTonn ajudou com o entendimento e realização do requisito
 function riscarTarefa(event) {
   const liList = event.target;
   liList.classList.toggle('completed');
@@ -49,6 +53,7 @@ buttonDelete.addEventListener('click', () => {
 });
 
 // APAGAR TAREFAS FINALIZADAS
+// referencia: @SrTonn ajudou com o entendimento e realização do requisito
 buttonRemFinal.addEventListener('click', () => {
   const tarefaCompleta = document.querySelectorAll('.completed');
   tarefaCompleta.forEach((element) => {
@@ -58,6 +63,34 @@ buttonRemFinal.addEventListener('click', () => {
 
 // REMOVER SELECIONADO;
 buttonRenSel.addEventListener('click', () => {
-  const tarefaSelecionada = document.querySelector('.selected');
-  tarefaSelecionada.remove();
+  const classSelected = document.querySelector('.selected');
+  classSelected.remove();
+});
+
+// SALVAR TAREFA
+
+// MOVER PARA CIMA;
+buttonMoverCima.addEventListener('click', () => {
+  // recuperar o li selecinoado
+  const classSelected = document.querySelector('.selected');
+  // se meu elemento selecionado(alguma li) for diferente que o pai(ol).filho na posição zero;
+  if (classSelected !== listaTarefas.children[0]) {
+  // criar uma variavel que ache o pai;
+    const searchFather = classSelected.parentNode;
+    // insertBefore tem dois paremetros, sendo o primeiro é "onde esta" e o segundo vai para "onde quer ir"
+    searchFather.insertBefore(classSelected, classSelected.previousElementSibling);
+  }
+});
+
+// MOVER PARA BAIXO;
+buttonMoverBaixo.addEventListener('click', () => {
+  // recuperar o li selecionado
+  const classSelected = document.querySelector('.selected');
+  // se meu elemento selecionado(alguma li) for diferente que o pai(ol).filho na última posição;
+  if (classSelected !== listaTarefas.lastChild) {
+  // criar uma variavel que ache o panpm run cypress:openi;
+    const searchFather = classSelected.parentNode;
+    // insertBefore tem dois paremetros, nesse caso sendo o primeiro "onde quero estar" e o segundo é "onde estou"
+    searchFather.insertBefore(classSelected.nextElementSibling, classSelected);
+  }
 });
